2d58a67b576687047d4d0c72c82b202d5cc09a6d
sji8562 <whaqlzkem@gmail.com>
2023-10-23 17:41:43 +0900

    JoinAlram init

---
+++ lib/ui/pages/home_page/join_alram_page/join_alram_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class JoinAlramPage extends StatelessWidget {
+  const JoinAlramPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
