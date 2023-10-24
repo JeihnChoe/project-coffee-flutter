4f1ef2e5d3c017e4fc4834e949f0159394df525e
qkrtjdud <qkrtjdudcjst@naver.com>
2023-10-24 10:14:44 +0900

    홈화면 그리기 수정 완

--- lib/ui/pages/home_page/home_main_page/widget/home_main_page_body.dart
+++ lib/ui/pages/home_page/home_main_page/widget/home_main_page_body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:project_coffee/_core/constants/size.dart';
 import 'package:project_coffee/_core/constants/style.dart';
+import 'package:project_coffee/ui/pages/home_page/home_main_page/widget/home_main_page_appbar.dart';
+import 'package:project_coffee/ui/pages/home_page/home_main_page/widget/home_main_page_banner.dart';
+import 'package:project_coffee/ui/pages/home_page/home_main_page/widget/home_main_page_body_item.dart';
 
 class HomeMainPageBody extends StatelessWidget {
   const HomeMainPageBody({super.key});
@@ -23,111 +26,13 @@ class HomeMainPageBody extends StatelessWidget {
                     child: textTitle1("안녕하세요, \n스타벅스 입니다."),
                   ),
                   SizedBox(height: gap_l),
-                  Card(
-                    elevation: 10, // 붕 떠 보이게 하는 효과(그림자 같은)
-                    child: Container(
-                      width: 600,
-                      height: 170,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(left: 30.0),
-                            child: textBody1(
-                                "스타벅스 리워드 회원\n신규가입 첫 구매 시,\n무료음료 혜택을 드려요!"),
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: <Widget>[
-                              Padding(
-                                padding: EdgeInsets.only(left: 30.0),
-                                child: ElevatedButton(
-                                  onPressed: () {
-                                    // 첫 번째 버튼을 눌렀을 때의 동작
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                  ),
-                                  child: Text("회원가입"),
-                                ),
-                              ),
-                              SizedBox(width: 10),
-                              ElevatedButton(
-                                onPressed: () {
-                                  // 두 번째 버튼을 눌렀을 때의 동작
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                ),
-                                child: Text("로그인"),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                  HomeMainPageBodyItem(),
                 ],
               ),
             ),
           ),
-          SliverAppBar(
-            pinned: true, // 스크롤시 핀 고정
-            expandedHeight: 40.0, // 높이 설정
-            centerTitle: false,
-            title: Align(
-              alignment: Alignment.centerLeft,
-              child: TextButton(
-                onPressed: () {},
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.mail_outline,
-                      color: Colors.black,
-                    ),
-                    SizedBox(width: gap_m), // 아이콘과 라벨 사이의 간격 조절
-                    textTitle2("what's New"),
-                  ],
-                ),
-              ),
-            ),
-
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(right: 16.0),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.add_alert_outlined,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(top: 5, left: 16, right: 16),
-                  child: Container(
-                    child: Column(
-                      children: <Widget>[
-                        Image.asset('assets/HomeMain-${index + 1}.png'),
-                      ],
-                    ),
-                  ),
-                );
-              },
-              childCount: 4, // 이미지 개수에 맞게 조정
-            ),
-          ),
+          HomeMainPageAppBar(),
+          HomeMainPageBanner(4),
         ],
       ),
     );
---
+++ lib/ui/pages/home_page/home_main_page/widget/home_main_page_body_item.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:project_coffee/_core/constants/style.dart';
+import 'package:project_coffee/ui/pages/home_page/join_page/join_page.dart';
+import 'package:project_coffee/ui/pages/home_page/login_page/login_page.dart';
+
+class HomeMainPageBodyItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10, // 붕 떠 보이게 하는 효과(그림자 같은)
+      child: Container(
+        width: 600,
+        height: 170,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 30.0),
+              child: textBody1("스타벅스 리워드 회원\n신규가입 첫 구매 시,\n무료음료 혜택을 드려요!"),
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 30.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => JoinPage()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Text("회원가입"),
+                  ),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginPage()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text("로그인"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
