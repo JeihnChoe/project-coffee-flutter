aad962d5c17e418e38de72550926a73263117a49
qkrtjdud <qkrtjdudcjst@naver.com>
2023-10-30 18:57:47 +0900

    이용내역

---
+++ lib/ui/pages/pay_page/pay_card_usage_details_page/pay_card_usage_details_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:project_coffee/ui/pages/pay_page/pay_card_usage_details_page/widget/pay_card_usage_detaIls_page_body.dart';
+
+class PayCardUsageDetailsPage extends StatelessWidget {
+  const PayCardUsageDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PayCardUsageDetailsPageBody();
+  }
+}
