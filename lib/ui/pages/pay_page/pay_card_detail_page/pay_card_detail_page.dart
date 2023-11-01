3d1cd656c5161b82d77371def91e507700d1013a
qkrtjdud <qkrtjdudcjst@naver.com>
2023-11-01 16:46:32 +0900

    홈화면 사진 추가

--- lib/ui/pages/pay_page/pay_card_detail_page/pay_card_detail_page.dart
+++ lib/ui/pages/pay_page/pay_card_detail_page/pay_card_detail_page.dart
@@ -8,6 +8,6 @@ class PayCardDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PayCadDetailPageBody(card);
+    return PayCardDetailPageBody(card);
   }
 }
