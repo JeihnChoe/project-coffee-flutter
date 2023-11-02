877e60491c4d72cc37e1ade23204635393044856
qkrtjdud <qkrtjdudcjst@naver.com>
2023-11-02 10:51:16 +0900

    카드 í통신중

--- lib/ui/pages/home_page/promotion_list_page/promotion_list_page_view_model.dart
+++ lib/ui/pages/home_page/promotion_list_page/promotion_list_page_view_model.dart
@@ -1,10 +1,9 @@
-//창고데이터
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:logger/logger.dart';
 import 'package:project_coffee/data/model/promotion.dart';
 import 'package:project_coffee/data/repository/promotion_repository.dart';
 import 'package:project_coffee/main.dart';
 
+//창고데이터
 class PromotionListModel {
   List<Promotion> promotionList;
   PromotionListModel(this.promotionList);
@@ -20,15 +19,15 @@ class PromotionListViewModel extends StateNotifier<PromotionListModel?> {
   Future<void> notifyInit() async {
     List<Promotion> responseDTO =
         await PromotionRepository().fetchPromotionDetailList();
-    Logger().d("통신?2");
+    //Logger().d("통신?2");
     state = PromotionListModel(responseDTO); // 수정된 부분
-    Logger().d("통신?4");
+    //Logger().d("통신?4");
   }
 }
 
 //창고관리자
 final promotionListProvider =
     StateNotifierProvider<PromotionListViewModel, PromotionListModel?>((ref) {
-  Logger().d("통신?33333333");
+  //Logger().d("통신?33333333");
   return PromotionListViewModel(PromotionListModel([]), ref)..notifyInit();
 });
--- lib/ui/pages/pay_page/pay_card_list_page/pay_card_list_page_view_model.dart
+++ lib/ui/pages/pay_page/pay_card_list_page/pay_card_list_page_view_model.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:project_coffee/data/dto/reponse_dto.dart';
+import 'package:logger/logger.dart';
 import 'package:project_coffee/data/model/paycard.dart';
 import 'package:project_coffee/data/repository/card_repository.dart';
 import 'package:project_coffee/main.dart';
@@ -14,16 +14,17 @@ class PayCardListModel {
 class PayCardListViewModel extends StateNotifier<PayCardListModel?> {
   final mContext = navigatorKey.currentContext;
   final Ref ref;
-  PayCardListViewModel(super.state, this.ref);
+  PayCardListViewModel(PayCardListModel? state, this.ref) : super(state);
 
   Future<void> notifyInit() async {
-    ResponseDTO responseDTO = await CardRepository().fetchCardDetailList();
-    state = PayCardListModel(responseDTO.response);
+    List<PayCard> responseDTO = await CardRepository().fetchCardDetailList();
+    Logger().d("통신하자");
+    state = PayCardListModel(responseDTO);
   }
 }
 
 //창고관리자
 final paycardListProvider =
     StateNotifierProvider<PayCardListViewModel, PayCardListModel?>((ref) {
-  return PayCardListViewModel(null, ref)..notifyInit();
+  return PayCardListViewModel(PayCardListModel([]), ref)..notifyInit();
 });
