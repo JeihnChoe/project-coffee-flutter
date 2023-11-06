90be5e4445bd05ff85b190bf845b5d693a4809df
sji8562 <whaqlzkem@gmail.com>
2023-11-06 21:25:54 +0900

    로그인 통신중

--- lib/data/store/session_store.dart
+++ lib/data/store/session_store.dart
@@ -7,15 +7,17 @@ import 'package:project_coffee/data/dto/reponse_dto.dart';
 import 'package:project_coffee/data/dto/user_request.dart';
 import 'package:project_coffee/data/repository/user_repository.dart';
 import 'package:project_coffee/main.dart';
+import 'package:project_coffee/ui/pages/home_page/home_login_change_main_page/home_login_change_main_page.dart';
+import 'package:project_coffee/ui/pages/main_page/main_page.dart';
 
 import '../model/user.dart';
 
 // 창고데이터
 class SessionUser {
   User? user;
-  // String? jwt;
-  // bool isLogin;
-  SessionUser({this.user});
+  String? jwt;
+  bool isLogin;
+  SessionUser({this.user, this.jwt, this.isLogin = false});
 }
 
 //창고
@@ -41,18 +43,18 @@ class SessionStore extends SessionUser {
     ResponseDTO responseDTO = await UserRepository().fetchLogin(loginReqDTO);
 
     // 2. 비지니스 로직
-    if (responseDTO.success == true) {
-      // 1. 세션값
-
+    if (responseDTO.response == 1) {
+      // 1. 세션값 갱신
       this.user = responseDTO.response as User;
-      // this.jwt = responseDTO.token;
-      // this.isLogin = true;
+      this.jwt = responseDTO.token;
+      this.isLogin = true;
 
       // 2. 디바이스에 JWT 저장 (자동 로그인)
-      // await secureStorage.write(key: "jwt", value: responseDTO.token);
-      // Logger().d("11111111111111111111111111${isLogin}");
+      await secureStorage.write(key: "jwt", value: responseDTO.token);
+
       // 3. 페이지 이동
-      Navigator.pushNamed(mContext!, Move.MainPage);
+
+      // Navigator.pushNamed(mContext!,Move.JoinSuccessPage,arguments: joinReqDTO);
     } else {
       ScaffoldMessenger.of(mContext!)
           .showSnackBar(SnackBar(content: Text(responseDTO.error)));
