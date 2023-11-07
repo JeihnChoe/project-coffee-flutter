5f68ed16c0ffc1074588c43877fcee4f4d2d7120
qkrtjdud <qkrtjdudcjst@naver.com>
2023-11-07 16:16:28 +0900

    텍스트버튼만 만들어짐

--- lib/ui/pages/order_page/shopping_cart_page/shopping_cart_page.dart
+++ lib/ui/pages/order_page/shopping_cart_page/shopping_cart_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:project_coffee/_core/constants/color.dart';
+import 'package:project_coffee/_core/constants/size.dart';
+import 'package:project_coffee/_core/constants/style.dart';
 
 import 'widget/shopping_cart_page_body.dart';
 
@@ -9,6 +13,38 @@ class ShoppingCartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ShoppingCartPageBody(),
+      persistentFooterButtons: [
+        Consumer(
+          builder: (context, ref, child) {
+            return Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    textBody1("총 / 20개"),
+                    textTitle1("원"),
+                  ],
+                ),
+                SizedBox(height: gap_m),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: kAccentColor,
+                    minimumSize: Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "주문하기",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ],
     );
   }
 }
