619c1a46ac973d2e1810a42d66b71394b304c46c
sji8562 <whaqlzkem@gmail.com>
2023-11-09 17:23:18 +0900

    로그인 통신완료

--- lib/data/model/user.dart
+++ lib/data/model/user.dart
@@ -3,26 +3,25 @@ class User {
   final String loginId;
   final String email; // 인증시 필요한 필드
   final String userName;
-  final String password;
+  String? password;
   final String phoneNumber;
   int? manager; // 1 : 유저 / 2 : 관리자
   String? createdAt; // 가입일시
   String? updatedAt;
 
   User(
-      { required this.loginId, required this.password,required this.email,required this.phoneNumber,required this.userName});
+      { required this.loginId,required this.email,required this.phoneNumber,required this.userName});
 
   User.fromJson(Map<String, dynamic> json)
       :
         loginId = json["loginId"],
-        password = json["password"],
         email = json["email"],
         phoneNumber = json["phoneNumber"],
         userName = json["userName"];
 
         Map<String, dynamic> toJson()=>{
     "loginId": loginId,
-    "password": password,
+    // "password": password,
     "email": email,
     "phoneNumber": phoneNumber,
     "userName": userName,
--- lib/ui/pages/order_page/beverage_detail_page/widget/bottom_sheet_outline_button_two.dart
+++ lib/ui/pages/order_page/beverage_detail_page/widget/bottom_sheet_outline_button_two.dart
@@ -11,7 +11,7 @@ class BottomSheetOutlineButtonTwo extends StatefulWidget {
   final ValueChanged<int> onStateChange;
 
   BottomSheetOutlineButtonTwo(
-      {required this.beverageOrderReqDTO,required this.selectedTabMethod,required this.count,required this.onStateChange});
+      {required this.beverageOrderReqDTO, required this.selectedTabMethod, required this.count, required this.onStateChange});
 
   int submit(int selectedPayment) {
     int selectedPaymentMethod = selectedPayment;
@@ -23,16 +23,17 @@ class BottomSheetOutlineButtonTwo extends StatefulWidget {
       _BottomSheetOutlineButtonState();
 }
 
-class _BottomSheetOutlineButtonState extends State<BottomSheetOutlineButtonTwo> {
+class _BottomSheetOutlineButtonState
+    extends State<BottomSheetOutlineButtonTwo> {
   int selectedPayment = 6;
-  int zeroprice= 6;
-  int oneprice= 7;
+  int zeroprice = 6;
+  int oneprice = 7;
 
   @override
   void initState() {
     selectedPayment = selectedPayment;
     zeroprice = widget.beverageOrderReqDTO.beverage.price;
-    oneprice= widget.beverageOrderReqDTO.beverage.price+500;
+    oneprice = widget.beverageOrderReqDTO.beverage.price + 500;
     super.initState();
   }
 
@@ -40,18 +41,19 @@ class _BottomSheetOutlineButtonState extends State<BottomSheetOutlineButtonTwo> 
   Widget build(BuildContext context) {
     return Row(
       children: [
-        if (widget.beverageOrderReqDTO.isIced == 1)
           Expanded(
             child: OutlinedButton(
               onPressed: () {
                 setState(() {
                   selectedPayment = 0;
                   widget.beverageOrderReqDTO.sizePrice = zeroprice;
-                  widget.beverageOrderReqDTO.totalmoney = zeroprice * widget.count;
+                  widget.beverageOrderReqDTO.totalmoney =
+                      zeroprice * widget.count;
                   widget.beverageOrderReqDTO.cup = 0;
                   widget.onStateChange(selectedPayment);
                   Logger().d(widget.beverageOrderReqDTO.cup);
-                  Logger().d("이거 안보여줘 ???${widget.beverageOrderReqDTO.totalmoney}");
+                  Logger().d(
+                      "이거 안보여줘 ???${widget.beverageOrderReqDTO.totalmoney}");
                 });
               },
               style: OutlinedButton.styleFrom(
@@ -110,12 +112,13 @@ class _BottomSheetOutlineButtonState extends State<BottomSheetOutlineButtonTwo> 
               setState(() {
                 selectedPayment = 1;
                 widget.beverageOrderReqDTO.sizePrice = oneprice;
-                widget.beverageOrderReqDTO.totalmoney = oneprice* widget.count;
+                widget.beverageOrderReqDTO.totalmoney = oneprice * widget.count;
 
                 widget.beverageOrderReqDTO.cup = 1;
                 widget.onStateChange(selectedPayment);
                 Logger().d(widget.beverageOrderReqDTO.cup);
-                Logger().d("이거 안보여줘 ???${widget.beverageOrderReqDTO.totalmoney}");
+                Logger().d(
+                    "이거 안보여줘 ???${widget.beverageOrderReqDTO.totalmoney}");
               });
             },
             style: OutlinedButton.styleFrom(
@@ -165,132 +168,7 @@ class _BottomSheetOutlineButtonState extends State<BottomSheetOutlineButtonTwo> 
             ),
           ),
         ),
-        SizedBox(
-          width: 10,
-        ),
-        Expanded(
-          child: OutlinedButton(
-            onPressed: () {
-              setState(() {
-                selectedPayment = 2;
-                widget.beverageOrderReqDTO.sizePrice = twoprice;
-                widget.beverageOrderReqDTO.totalmoney = twoprice* widget.count;
-                widget.beverageOrderReqDTO.cup = 2;
-                widget.onStateChange(selectedPayment);
-                Logger().d(widget.beverageOrderReqDTO.cup);
-                Logger().d("이거 안보여줘 ???${widget.beverageOrderReqDTO.totalmoney}");
-              });
-            },
-            style: OutlinedButton.styleFrom(
-              primary: selectedPayment == 2 ? kAccentColor : Colors.black,
-              side: BorderSide(
-                color: selectedPayment == 2 ? kAccentColor : Colors.grey,
-                width: 1.0,
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-            ),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Column(
-                  children: [
-                    SizedBox(height: 16),
-                    Container(
-                      width: 40,
-                      child: Image.asset(
-                        "assets/grande.jpg",
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    SizedBox(height: gap_m),
-                    Text("Grande"),
-                    SizedBox(height: gap_s),
-                    Text("473ml"),
-                    SizedBox(height: gap_l),
-                  ],
-                ),
-                if (selectedPayment == 2)
-                  Positioned(
-                    top: 25, // 위로 올릴 값
-                    left: 10,
-                    child: Container(
-                      width: 16,
-                      height: 16,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.green,
-                      ),
-                    ),
-                  ),
-              ],
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 10,
-        ),
-        Expanded(
-          child: OutlinedButton(
-            onPressed: () {
-              setState(() {
-                selectedPayment = 3;
-                widget.beverageOrderReqDTO.sizePrice = threeprice;
-                widget.beverageOrderReqDTO.totalmoney = threeprice* widget.count;
-                widget.beverageOrderReqDTO.cup = 3;
-                widget.onStateChange(selectedPayment);
-                Logger().d(widget.beverageOrderReqDTO.cup);
-                Logger().d("이거 안보여줘 ???${widget.beverageOrderReqDTO.totalmoney}");
-              });
-            },
-            style: OutlinedButton.styleFrom(
-              primary: selectedPayment == 3 ? kAccentColor : Colors.black,
-              side: BorderSide(
-                color: selectedPayment == 3 ? kAccentColor : Colors.grey,
-                width: 1.0,
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-            ),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Column(
-                  children: [
-                    SizedBox(height: 16),
-                    Container(
-                      width: 40,
-                      child: Image.asset(
-                        "assets/venti.jpg",
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    SizedBox(height: gap_m),
-                    Text("Venti"),
-                    SizedBox(height: gap_s),
-                    Text("591ml"),
-                    SizedBox(height: gap_l),
-                  ],
-                ),
-                if (selectedPayment == 3)
-                  Positioned(
-                    top: 25, // 위로 올릴 값
-                    left: 10,
-                    child: Container(
-                      width: 16,
-                      height: 16,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.green,
-                      ),
-                    ),
-                  ),
-              ],
-            ),
-          ),
-        )
+
       ],
     );
   }
