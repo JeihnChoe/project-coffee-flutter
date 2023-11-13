e0887671b71c8d037660f8628fc8ad6e852a31a0
sji8562 <whaqlzkem@gmail.com>
2023-11-14 00:32:11 +0900

    만들긴 만들었는데 아직 기능 확인 안함

--- lib/ui/pages/order_page/category_list_page/widget/category_list_page_body.dart
+++ lib/ui/pages/order_page/category_list_page/widget/category_list_page_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:project_coffee/data/dto/order_request.dart';
 import 'package:project_coffee/data/store/session_store.dart';
+import 'package:project_coffee/ui/pages/home_page/login_page/login_page.dart';
 import 'package:project_coffee/ui/pages/order_page/%20select_order_store_page/select_order_store_list_page.dart';
 import 'package:project_coffee/ui/pages/order_page/shopping_cart_page/shopping_cart_page.dart';
 import 'package:project_coffee/ui/pages/order_page/shopping_cart_page/shopping_cart_page_view_model.dart';
@@ -25,7 +26,7 @@ class CategoryListPageBody extends StatelessWidget {
             CategoryListPageAppBar(title: "Order"),
             SliverToBoxAdapter(
               child:
-                  CustomTabBar(contentTitle1: "전체 메뉴", contentTitle2: "나만의 메뉴"),
+              CustomTabBar(contentTitle1: "전체 메뉴", contentTitle2: "나만의 메뉴"),
             ),
             SliverFillRemaining(
               child: TabBarView(children: [
@@ -59,34 +60,51 @@ class CategoryListPageBody extends StatelessWidget {
                 decoration: ShapeDecoration(
                   color: Colors.black, // 배경색 설정
                   shape:
-                      RoundedRectangleBorder(), // 원 모양의 배경을 만듭니다. 원 모양이 아니라면 다른 모양을 선택할 수 있습니다.
+                  RoundedRectangleBorder(), // 원 모양의 배경을 만듭니다. 원 모양이 아니라면 다른 모양을 선택할 수 있습니다.
                 ),
                 child: Consumer(
                   builder: (context, ref, child) {
-                    SessionUser sessionUser = ref.read(sessionProvider);
-                    String? jwt = sessionUser.jwt;
+                    SessionUser? sessionUser = ref.read(sessionProvider);
+                    String? jwt = sessionUser?.jwt;
 
-                    ShoppingCartListModel? model =
-                        ref.watch(shoppingCartListProvider(jwt!));
-                    List<CartTotalDTO> cartTotalList =
-                        model?.cartTotalDTO ?? [];
-                    return IconButton(
-                      icon: Icon(CupertinoIcons.bag),
-                      color: Colors.white, // 아이콘 색상 설정
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
+                    if (jwt != null) {
+                      ShoppingCartListModel? model = ref.watch(shoppingCartListProvider(jwt));
+                      List<CartTotalDTO> cartTotalList = model?.cartTotalDTO ?? [];
+
+                      return IconButton(
+                        icon: Icon(CupertinoIcons.bag),
+                        color: Colors.white,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
                               builder: (context) => ShoppingCartPage(
-                                  cartTOtalDTO: cartTotalList)),
-                        );
-                      },
-                    );
+                                cartTOtalDTO: cartTotalList,
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      // jwt가 null일 때 수행할 코드 또는 예외 처리
+                      return IconButton(
+                        icon: Icon(CupertinoIcons.bag),
+                        color: Colors.white,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ),
+                          );
+                        },
+                      );
+                    }
                   },
                 ),
-              )
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
